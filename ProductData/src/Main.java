ef715a44763752f8be97c87764b35b4d1aa2c9af
Wilson Simanjuntak <wilsontulus5@gmail.com>
2025-10-06 12:18:34 +0700

    Import Java code and Swing form

--- ProductData/src/Main.java
+++ ProductData/src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        // Buat object window
+        ProductMenu menu = new ProductMenu();
+
+        // Set ukuran window
+        menu.setSize(700, 600);
+
+        // Letakkan window di tengah layar
+        menu.setLocationRelativeTo(null);
+
+        // Isikan window
+
+
+    }
 }
